10c42df6fd40344f2332c1e4206617e84f2be771
LluisPM <1746993@uab.cat>
2024-11-08 02:11:44 +0100

    Update practica2.c
    biblioteca  acabada

--- practica2.c
+++ practica2.c
@@ -26,18 +26,18 @@ void InitData(){
         V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
         V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
     }
-}
+    }
 
 void PrintVect( float vect[N], int from, int numel )
 {
     for (int i=from; i<=numel; i++){
-        printf("%f\n",vect[i]);
+        printf("%f ",vect[i]);
     }
 }
 
 void PrintRow( float mat[N][N], int row, int from, int numel ){
   for (int i=from; i<=numel; i++){
-    printf("%f\n",mat[row][i]);
+    printf("%f ",mat[row][i]);
   }
  }
 void MultEscalar( float vect[N], float vectres[N], float alfa ){
@@ -114,15 +114,14 @@ float sum=0.0;
   return sqrt(sum);
 }
 int DiagonalDom( float M[N][N] ){
-float sum_element_diagonal=0.0;
 for (int i=0; i < N; i++){
+    float sum_element_diagonal=0.0;
   for (int j=0; j < N; j++){
-    if (i =! j){
+    if (i != j){
     sum_element_diagonal+=fabs(M[i][j]);
     }
     }
-
-  if (M[i][i]<sum_element_diagonal){
+  if (fabs(M[i][i])<sum_element_diagonal){
     return 0;
     }
   }
@@ -137,8 +136,104 @@ void Matriu_x_Vector( float M[N][N], float vect[N], float vectres[N] ){
         }
     }
   }
+
+int Jacobi(float M[N][N], float vect[N], float vectres[N], unsigned iter) {
+for(int i=0; i < N; i++){
+vectres[i]=0.0;
+}
+    if (DiagonalDom(M)==1) {
+        for (unsigned k = 0; k < iter; k++) {
+
+            for (int i = 0; i < N; i++) {
+                float sum = 0.0;
+
+                for (int j = 0; j < N; j++) {
+                    if (i != j) {
+                        sum -= M[i][j] * vectres[j];
+                    }
+                }
+
+
+                vectres[i] = (vect[i] - sum) / M[i][i];
+            }
+
+
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main(){
-    InitData();
-    PrintRow(MatDD,0,0,9);
-    PrintRow(MatDD,100,95,104);
+InitData();
+float vectres[N];
+printf("D.\n\n");
+printf("Infininorma de Mat = %f\n",Infininorm(Mat));
+printf("Norma u de Mat = %f\n",Onenorm(Mat));
+printf("Norma de Forbenius = %f\n",NormFrobenius(Mat));
+    if (DiagonalDom(Mat)==1){
+    printf("La matriu Mat és diagonal dominant.\n");
+    } else {
+    printf("La matriu Mat no és diagonal dominant.\n");
+}
+printf("\n");
+    printf("Infininorma de MatDD = %f\n",Infininorm(MatDD));
+    printf("Norma u de MatDD = %f\n",Onenorm(MatDD));
+    printf("Norma de Forbenius = %f\n",NormFrobenius(MatDD));
+    if (DiagonalDom(MatDD)==1){
+        printf("La matriu MatDD és diagonal dominant.\n");
+    } else {
+        printf("La matriu MatDD no és diagonal dominant.\n\n");
+    }
+    printf("E.\n\n");
+printf("Escalar <V1,V2> = %f Escalar <V1,V3> = %f Escalar <V2,V3> = %f\n\n",Scalar(V1,V2),Scalar(V1,V3),Scalar(V2,V3));
+printf("F.\n\n");
+printf("Magnitud V1,V2 i V3 = %f %f %f\n\n",Magnitude(V1),Magnitude(V2),Magnitude(V3));
+printf("G.\n\n");
+if (Ortogonal(V1,V2)==1){
+printf("V1 i V2 són ortogonals\n");
+}
+if (Ortogonal(V1,V3)==1){
+printf("V1 i V3 són ortogonals\n");
+}
+if (Ortogonal(V2,V3)==1){
+printf("V2 i V3 són ortogonals\n");
+}
+printf("\n");
+printf("H.\n\n");
+MultEscalar(V3,vectres,2.0);
+printf("Els elements 0 al 9 i 256 al 265 del resultat de multiplicar V3x2.0 són:\n");
+PrintVect( vectres, 0, 9 );
+printf("\n");
+PrintVect( vectres, 256, 265 );
+printf("\n\n");
+printf("I.\n\n");
+Projection( V2, V3,vectres );
+printf("Els elements 0 a 9 del resultat de la projecció de V2 sobre V3 són:\n");
+PrintVect( vectres, 0, 9 );
+printf("\n");
+Projection(V1,V2,vectres);
+printf("Els elements 0 a 9 del resultat de la projecció de V1 sobre V2 són:\n");
+PrintVect( vectres, 0, 9 );
+printf("\n\n");
+printf("J.\n\n");
+Matriu_x_Vector( Mat,V2,vectres );
+printf("Els elements 0 a 9 del resultat de la multiplicació de Mat per v2 són:\n");
+PrintVect( vectres, 0, 9 );
+printf("\n\n");
+
+printf("K.\n\n");
+Jacobi(MatDD,V3,vectres,1);
+printf("Els elements 0 a 9 de la solució (1 iter) del sistema d'equacions són:\n");
+PrintVect( vectres, 0, 9 );
+printf("\n");
+Jacobi(MatDD,V3,vectres,1000);
+printf("Els elements 0 a 9 de la solució (1000 iters) del sistema d'equacions són:\n");
+PrintVect( vectres, 0, 9 );
+printf("\n");
+
+
+
+
 }
